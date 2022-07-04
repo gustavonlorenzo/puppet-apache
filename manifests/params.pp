14b0bb9a1606bc3d2143ed66cb56d2a08a37dd57
Gustavo Lorenzo <gustavonlorenzo@gmail.com>
2022-07-04 10:36:11 -0400

    extending apache class

---
+++ manifests/params.pp
@@ -0,0 +1,11 @@
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
