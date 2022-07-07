ff889e4b02e13d1d7c566105aeb238da661e1495
Gustavo Lorenzo <gustavonlorenzo@gmail.com>
2022-07-06 20:30:29 -0400

    test

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,7 @@ class apache::config {
   file { 'apache_config': 
     ensure => $apache::config_ensure,
     path   => $apache::config_path,
-    source => "puppet:///modules/apache/files/${osfamily}.conf",
+    source => "puppet:///modules/apache/files/${facts.os.family}.conf",
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
