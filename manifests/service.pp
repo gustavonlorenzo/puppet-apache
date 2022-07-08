451f3fbae82a7ddb2fc876ad06b474fb57fb19df
Gustavo Lorenzo <gustavonlorenzo@gmail.com>
2022-07-08 11:33:21 -0400

    t

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,10 @@
 # Allows for apache service to restart when triggered.
 class apache::service {
-  service { "${apache::service_name}"}
+  service { "${apache::service_name}":
     alias      => 'apache_service',
     ensure     => $apache::service_ensure,
     enable     => $apache::service_enable,
     hasrestart => true,
+  }
+
 }
