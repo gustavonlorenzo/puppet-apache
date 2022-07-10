44ad84e2915251e5228ba4827e1d0bf41631f98b
Gustavo Lorenzo <gustavonlorenzo@gmail.com>
2022-07-10 10:33:59 -0400

    t

--- lib/facter/apache_processes.rb
+++ lib/facter/apache_processes.rb
@@ -1,5 +1,10 @@
 Facter.add(:apache_processes) do
+  context :kernel => "Linux"
   setcode do
-   
+    osfamily = Facter.value(:osfamily)
+    case osfamily
+    when 'Debian'
+      Facter::Core::Execution.execute('pgrep apache2 | wc -l')
+    end
   end
 end
