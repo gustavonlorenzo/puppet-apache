103e28e0d0f08202a55ba69d90806b7a860361e5
gustavonlorenzo <gustavonlorenzo@gmail.com>
2022-07-17 11:02:35 -0400

    t

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,13 +5,13 @@
 # @example
 #   include apache::params
 class apache::params {
-  $install_ensure = 'present',
-  case $::osfamily: {
+  $install_ensure = 'present'
+  case $::osfamily {
     'RedHat': {
-      $install_name = 'httpd',
+      $install_name = 'httpd'
     }
     'Debian': {
-      $install_name = 'apache2',
+      $install_name = 'apache2'
     }
   }
 }
